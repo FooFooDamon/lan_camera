a9fe74454066353751a870ad77206da8f9ed8352
FooFooDamon <udc577@126.com>
2026-04-18 21:04:01 +0800

    scripts/aliases.sh: Run a script instead of running an executable file to start the client or server.

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,3 +1,3 @@
-alias lanc_client='screen -d -m -U -S lanc_client -t lanc_client -h 100000 -L -Logfile /tmp/lanc_client.log bash -c "time lanc_client -c ~/etc/lan_camera.cli.json"'
-alias lanc_server='screen -d -m -U -S lanc_server -t lanc_server -h 100000 -L -Logfile /tmp/lanc_server.log bash -c "time lanc_server -c ~/etc/lan_camera.srv.json"'
+alias lanc_client='screen -d -m -U -S lanc_client -t lanc_client -h 100000 -L -Logfile /tmp/lanc_client.log bash -c "time bash /opt/lan_camera/scripts/lanc_client.sh"'
+alias lanc_server='screen -d -m -U -S lanc_server -t lanc_server -h 100000 -L -Logfile /tmp/lanc_server.log bash -c "time bash /opt/lan_camera/scripts/lanc_server.sh"'
 
