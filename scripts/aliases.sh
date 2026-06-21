d432fbc74efa2d4c9eabcaafbf428deee383eb48
FooFooDamon <udc577@126.com>
2026-06-21 09:54:27 +0800

    scripts/aliases.sh: + lanc_{client,server}.sh, test_lanc_{client,server}.sh

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,3 +1,7 @@
 alias lanc_client='screen -d -m -U -S lanc_client -t lanc_client -h 100000 -L -Logfile /tmp/lanc_client.log bash -c "time bash /opt/lan_camera/scripts/lanc_client.sh"'
 alias lanc_server='screen -d -m -U -S lanc_server -t lanc_server -h 100000 -L -Logfile /tmp/lanc_server.log bash -c "time bash /opt/lan_camera/scripts/lanc_server.sh"'
+alias lanc_client.sh='time bash /opt/lan_camera/scripts/lanc_client.sh'
+alias lanc_server.sh='time bash /opt/lan_camera/scripts/lanc_server.sh'
+alias test_lanc_client.sh='time bash /opt/lan_camera/scripts/test_lanc_client.sh'
+alias test_lanc_server.sh='time bash /opt/lan_camera/scripts/test_lanc_server.sh'
 
