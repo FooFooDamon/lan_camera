27a5a2aeaa5545bcee31ae3d60839f236add2061
FooFooDamon <udc577@126.com>
2026-09-16 10:45:44 +0800

    scripts/aliases.sh: Add comment for showing how to start the server in /etc/rc.local

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,4 +1,5 @@
 alias lanc_client='screen -d -m -U -S lanc_client -t lanc_client -h 100000 -L -Logfile /tmp/lanc_client.log bash -c "time bash /opt/lan_camera/scripts/lanc_client.sh"'
+# A demo for /etc/rc.local: sudo -i -u myuser screen ...
 alias lanc_server='screen -d -m -U -S lanc_server -t lanc_server -h 100000 -L -Logfile /tmp/lanc_server.log bash -c "time bash /opt/lan_camera/scripts/lanc_server.sh"'
 alias lanc_client.sh='time bash /opt/lan_camera/scripts/lanc_client.sh'
 alias lanc_server.sh='time bash /opt/lan_camera/scripts/lanc_server.sh'
